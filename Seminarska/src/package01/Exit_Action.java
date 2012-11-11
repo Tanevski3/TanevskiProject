be7adf5d9c3ce5883a51338c82b83d733e2f0dcf
Marjan Tanevski <marjantanevski@gmail.com>
2012-11-11 12:29:25 +0100

    Dodavanje na proektot vo GitHub

---
+++ Seminarska/src/package01/Exit_Action.java
@@ -0,0 +1,16 @@
+package package01;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Exit_Action implements ActionListener { //sto ke se sluci sekogas koga ke bidi pritisnato podmenito exit
+
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		System.exit(0); 
+		
+	} 
+
+}
